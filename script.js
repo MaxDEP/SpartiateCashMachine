918341d1b46ba28ec92b1c46a50d62e2d3754a45
MaxSliw <anthony.progi@gmail.com>
2018-03-07 08:59:00 +0100

    Début liste des gains

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let rouleaux = document.getElementById('rouleaux');
+let gains = {
+    "x2triple": {
+        image: "",
+        resultat: 1200
+    },
+    "triple7rouge": {
+        image: "",
+        resultat: 100
+    },
+    "triple7rose": {
+        image: "",
+        resultat: 80
+    },
+    "triple7bleu": {
+        image: "",
+        resultat: 60
+    },
+    "any7": {
+        image: "",
+        resultat: 30
+    },
+    "triple5": {
+        image: "",
+        resultat: 50
+    }
+}
