cdac73bb1e708f1fd0a17be6ec1dbadbd3b97657
MaxSliw <anthony.progi@gmail.com>
2018-03-07 09:55:56 +0100

    Gains et points

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 'use strict';
 
-let rouleaux = document.getElementById('rouleaux');
-let gains = {
-    "x2triple": {
-        image: "",
-        resultat: 1200
-    },
-    "triple7rouge": {
-        image: "",
-        resultat: 100
-    },
-    "triple7rose": {
-        image: "",
-        resultat: 80
-    },
-    "triple7bleu": {
-        image: "",
-        resultat: 60
-    },
-    "any7": {
-        image: "",
-        resultat: 30
-    },
-    "triple5": {
-        image: "",
-        resultat: 50
+let html_rouleaux = document.getElementById('rouleaux');
+let img1 = 0,
+    img2 = 0,
+    img3 = 0;
+let html_points = document.getElementById('points');
+let points = 90;
+let gains = JSON.parse(gains_data);
+let amiser = false;
+
+updateVue();
+
+function updateVue() {
+    html_points.innerHTML = "Points : " + points;
+    html_rouleaux.children[0].innerText = img1;
+    html_rouleaux.children[1].innerText = img2;
+    html_rouleaux.children[2].innerText = img3;
+}
+
+function miser() {
+    if (!amiser) {
+        points--;
+        amiser = true;
+        updateVue();
+        console.log("Mise OK");
+    }
+}
+
+function jouer() {
+    if (amiser) {
+        console.log("je joue");
+        amiser = false;
+        img1 = Math.floor((Math.random() * 5) + 1);
+        img2 = Math.floor((Math.random() * 5) + 1);
+        img3 = Math.floor((Math.random() * 5) + 1);
+        console.log(img1 + " " + img2 + " " + img3);
+        updateGains();
+        updateVue();
+    } else {
+        alert("Vous devez miser");
     }
 }
