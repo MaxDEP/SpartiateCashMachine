10b21d413f228050e46a6f5180bba31161557068
MaxSliw <anthony.progi@gmail.com>
2018-03-14 12:06:02 +0100

    Tourni et design

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let html_rouleaux = document.getElementById('rouleaux');
-let html_points = document.getElementById('points');
-let html_mise = document.getElementById('mise');
+let html_points = document.getElementById('affichage_points');
+let html_mise = document.getElementById('affichage_mise');
 let img1 = 0,
     img2 = 0,
     img3 = 0;
@@ -10,42 +10,78 @@ let points = 90;
 let mise = 0;
 let gains_data = gains;
 let amiser = false;
+let images = [
+    "./media/cerise.png",
+    "./media/seven.png",
+    "./media/cloche.png",
+    "./media/bar.png",
+    "./media/croix.png"
+]
+let peutJouer = true;
+let peutActualiser = false;
 
-updateVue();
+function tourni() {
+    let img1_tourni = Math.floor((Math.random() * 5) + 1);
+    let img2_tourni = Math.floor((Math.random() * 5) + 1);
+    let img3_tourni = Math.floor((Math.random() * 5) + 1);
+    html_rouleaux.children[0].children[0].src = images[img1_tourni - 1];
+    html_rouleaux.children[1].children[0].src = images[img2_tourni - 1];
+    html_rouleaux.children[2].children[0].src = images[img3_tourni - 1];
+}
 
 function updateVue() {
-    html_points.innerHTML = "Points : " + points;
-    html_mise.innerHTML = "Mise : " + mise;
-    html_rouleaux.children[0].innerText = img1;
-    html_rouleaux.children[1].innerText = img2;
-    html_rouleaux.children[2].innerText = img3;
+    if(!peutActualiser) {
+        html_mise.innerHTML = "<p>" + mise + "</p>";
+    } else {
+        html_mise.innerHTML = "<p>" + mise + "</p>";
+        let yolo_tourni = setInterval(tourni, 950);
+        setTimeout(function () {
+            clearInterval(yolo_tourni);
+            html_rouleaux.children[0].children[0].src = images[img1 - 1];
+            html_rouleaux.children[1].children[0].src = images[img2 - 1];
+            html_rouleaux.children[2].children[0].src = images[img3 - 1];
+            peutJouer = true;
+            peutActualiser = false;
+            html_points.innerHTML = "<p>" + points + "</p>";
+        }, 3500);
+    }
 }
+
 function miserMax() {
     amiser = true;
     mise = 3;
-    updateVue();
-    jouer();
+    if (peutJouer) {
+        peutActualiser = true;
+        updateVue();
+        jouer();
+        peutJouer = false;
+    }
 }
 
 function miser() {
     amiser = true;
     mise++;
-    if(mise > 3){
+    if (mise > 3) {
         mise = 1;
     }
     updateVue();
+    peutJouer = true;
 }
 
 function jouer() {
     if (amiser) {
-        console.log("je joue");
-        points = points - mise;
-        img1 = Math.floor((Math.random() * 5) + 1);
-        img2 = Math.floor((Math.random() * 5) + 1);
-        img3 = Math.floor((Math.random() * 5) + 1);
-        console.log(img1 + " " + img2 + " " + img3);
-        updateGains();
-        updateVue();
+        if (peutJouer) {
+            console.log("je joue");
+            points = points - mise;
+            img1 = Math.floor((Math.random() * 5) + 1);
+            img2 = Math.floor((Math.random() * 5) + 1);
+            img3 = Math.floor((Math.random() * 5) + 1);
+            console.log(img1 + " " + img2 + " " + img3);
+            peutJouer = false;
+            peutActualiser = true;
+            updateGains();
+            updateVue();
+        }
     } else {
         alert("Vous devez miser");
     }
@@ -54,7 +90,7 @@ function jouer() {
 function updateGains() {
     for (let suite in gains) {
         if (gains[suite].img1 == img1 && gains[suite].img2 == img2 && gains[suite].img3 == img3) {
-            points += gains[suite].resultat*mise;
+            points += gains[suite].resultat * mise;
         }
     }
     console.log("Points : " + points + " et la mise " + mise);
