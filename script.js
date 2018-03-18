3b120c099f8a31a6ce9aefce9369245f02493590
Ughetto <bragua84@gmail.com>
2018-03-18 19:21:46 +0100

    tourni

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ var images = [
 ];
 
 // Images random dans les rouleux
-function tourni() {
+/*function tourni() {
     let img1_tourni = Math.floor((Math.random() * 5) + 1);
     let img2_tourni = Math.floor((Math.random() * 5) + 1);
     let img3_tourni = Math.floor((Math.random() * 5) + 1);
@@ -58,6 +58,44 @@ function updateVue() {
             peutActualiser = false;
             html_points.innerHTML = "<p>" + points + "</p>";
         }, 3500);
+    }
+}*/
+
+function tourni(i){
+    if(i == 60){
+        html_rouleaux.children[2].children[0].src = images[img3 - 1];
+        peutJouer = true;
+        peutActualiser = false;
+        html_points.innerHTML = "<p>" + points + "</p>";
+    }else if(i >= 50){
+        let img3_tourni = Math.floor((Math.random() * 5) + 1);
+        html_rouleaux.children[2].children[0].src = images[img3_tourni - 1];
+        html_rouleaux.children[1].children[0].src = images[img2 - 1];
+        setTimeout(tourni, 400, i+1);
+    }else if(i >= 30){
+        let img2_tourni = Math.floor((Math.random() * 5) + 1);
+        let img3_tourni = Math.floor((Math.random() * 5) + 1);
+        html_rouleaux.children[1].children[0].src = images[img2_tourni - 1];
+        html_rouleaux.children[2].children[0].src = images[img3_tourni - 1];
+        html_rouleaux.children[0].children[0].src = images[img1 - 1];
+        setTimeout(tourni, 250, i+1);
+    }else{
+        let img1_tourni = Math.floor((Math.random() * 5) + 1);
+        let img2_tourni = Math.floor((Math.random() * 5) + 1);
+        let img3_tourni = Math.floor((Math.random() * 5) + 1);
+        html_rouleaux.children[0].children[0].src = images[img1_tourni - 1];
+        html_rouleaux.children[1].children[0].src = images[img2_tourni - 1];
+        html_rouleaux.children[2].children[0].src = images[img3_tourni - 1];
+        setTimeout(tourni, 100, i+1);
+    }
+}
+
+function updateVue() {
+    if (!peutActualiser) {
+        html_mise.innerHTML = "<p>" + mise + "</p>";
+    }else{
+        html_mise.innerHTML = "<p>" + mise + "</p>";
+        tourni(0);
     }
 }
 
